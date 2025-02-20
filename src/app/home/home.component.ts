d4940d423d0c26d38542e2ab240aa4ce02135ee2
Yves Braeckman <yves.braeckman.06@gmail.com>
2025-02-20 10:34:52 +0100

    kobe suckt

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { MenuComponent } from "../menu/menu.component";
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [MenuComponent],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.css'
+})
+export class HomeComponent {
+
+}
---
+++ src/app/menu/menu.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-menu',
+  standalone: true,
+  imports: [MenuComponent],
+  templateUrl: './menu.component.html',
+  styleUrl: './menu.component.css'
+})
+export class MenuComponent {
+
+}
